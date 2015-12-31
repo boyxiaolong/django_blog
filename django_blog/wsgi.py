806f27e6883d6fc9d0aa6d158ad85165c523f4cc
boyxiaolong <boyhailong@gmai.com>
2015-12-31 15:57:50 +0800

    add

--- django_blog/wsgi.py
+++ django_blog/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
